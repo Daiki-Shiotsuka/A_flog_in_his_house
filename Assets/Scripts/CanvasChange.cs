6fefa1c0d3f7bc9f89ac5c2bb3463eaa76264d4d
Daiki-Shiotsuka <64032115+Daiki-Shiotsuka@users.noreply.github.com>
2021-07-07 19:58:23 +0900

    Update CanvasChange.cs

--- Assets/Scripts/CanvasChange.cs
+++ Assets/Scripts/CanvasChange.cs
@@ -40,7 +40,7 @@ public class CanvasChange : MonoBehaviour {
     int RealTime()
     {
         int D0 = 9;
-        int H0 = 0;
+        int H0 = 11;
         int M0 = 0;
         int S0 = 0;
         int starttime = 0;
